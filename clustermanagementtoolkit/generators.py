7cb13b741e3126e34b4e23af8e23a95fe36d9725
David Weinehall <tao@debian.org>
2026-03-27 23:12:15 +0200

    generators: Handle checksum prefixes
    Support hashsums in the hex formatter.
    
    Signed-off-by: David Weinehall <tao@debian.org>

--- clustermanagementtoolkit/generators.py
+++ clustermanagementtoolkit/generators.py
@@ -687,8 +687,12 @@ def generator_hex(obj: dict, field: str, fieldlen: int, pad: bool,
     if (tmp := format_special(string, selected)) is not None:
         array = [tmp]
     else:
-        array = format_numerical_with_units(string, "numerical",
-                                            selected, non_units=set("0123456789abcdefABCDEF"))
+        if string.startswith(("sha224", "sha256", "sha384", "sha512")):
+            tmp = string.split(":", maxsplit=1)
+            array += [ThemeStr(tmp[0], ThemeAttr("main", "highlight"))]
+            string = f":{tmp[1]}"
+        array += format_numerical_with_units(string, "numerical",
+                                             selected, non_units=set("0123456789abcdefABCDEF"))
 
     return align_and_pad(array, fieldlen=fieldlen, pad=pad, ralign=ralign, selected=selected)
 
